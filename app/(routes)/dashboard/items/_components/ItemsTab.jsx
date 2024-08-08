c3554c98f3fa1f90b196ca90d0ea1749f496c1a2
sallauddin3 <sallauddin3@gatech.edu>
2024-08-08 13:19:41 -0400

    Fixing search

--- app/(routes)/dashboard/items/_components/ItemsTab.jsx
+++ app/(routes)/dashboard/items/_components/ItemsTab.jsx
@@ -109,17 +109,3 @@ function ItemsTab() {
 }
 
 export default ItemsTab
-/**
- *  {itemsList?.length>0?
-                <ItemListTable
-                itemsList= {getDisplayedItems()}
-                refreshData={() => {
-                    getInventoryList();
-                    getAllItems();
-                }} />
-            :
-                [1, 2, 3, 4, 5].map((item, index) => (
-                    <div key={index} className='w-full bg-slate-200 rounded-lg h-[100px] animate-pulse'></div>
-                ))
-            }
- */
